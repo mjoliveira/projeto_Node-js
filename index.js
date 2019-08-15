6001e8438cbddf811e0ca41090cb76399d1822f7
mjoliveira <mayara.jesus@acad.pucrs.br>
2019-08-15 19:04:18 -0300

    Formata saida do nome e implementa uso de uml e querystring

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const faker = require('faker');
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
 const server = http.createServer();
 
 server.on('request', function (request, response) {
-  const randomName = faker.name.findName();
+  console.log('request.url:', request.url);
 
-  response.write('Seu nome randomico: ' + randomName);
+  const parsedUrl = url.parse(request.url);
+  const parameters = querystring.parse(parsedUrl.query);
+
+  console.log('parameters:', parameters);
+
+  const nomeRandomico = faker.name.findName(parameters.template);
+  const nomeRandomicoFormatado = faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}");
+
+  response.write('Seu nome randômico: ' + nomeRandomico + '\n');
+  response.write('Seu nome randômico formatado: ' + nomeRandomicoFormatado + '\n');
   response.end();
 });
 
